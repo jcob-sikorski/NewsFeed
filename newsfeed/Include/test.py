7e63ef037d695df87992ce830c327abfbecc5465
jcob-sikorski <jm.siekiera@gmail.com>
2020-04-09 18:29:49 +0200

    testing sorting functionality by sorting tweets initially  by likes

--- newsfeed/Include/test.py
+++ newsfeed/Include/test.py
@@ -1,11 +1,7 @@
 import json
 from jsonLoad import jsonLoad
-from jsonSave import jsonSave
 
 friends = jsonLoad('friends_data.json')
-sorted_ids_by_likes = []
-
-each_user_sorted_ids={}
 
 for screen_name in friends['friends']:
     dir_path = r'C:\Users\jmsie\Dev\Projects\NewsFeed\data'
@@ -14,33 +10,4 @@ for screen_name in friends['friends']:
     path = ''.join(dir_path + f'\{screen_name}_tweets.json')
 
     user_data = jsonLoad(path)
-
-    # IDs
-    keys = [*user_data]
-
-    likes = {}
-
-    # make dictionary with ID: likes pair only
-    for i in keys:
-        likes.update({i: user_data[i]['favorite_count']})
-
-    # sort IDs by likes count
-    likes = {key: value for key, value in reversed(sorted(likes.items(), key=lambda item: item[1]))}
-
-    #sorted_ids_by_likes.extend(likes.keys())
-    each_user_sorted_ids[screen_name] = [key for key in likes.keys()]
-
-jsonSave('tweetsSortedByLikes', each_user_sorted_ids)
-
-tweetsSortedByLikes = jsonLoad('tweetsSortedByLikes')
-
-for screen_name in friends['friends']:
-    dir_path = r'C:\Users\jmsie\Dev\Projects\NewsFeed\data'
-
-    # path to new data
-    path = ''.join(dir_path + f'\{screen_name}_tweets.json')
-
-    user_data = jsonLoad(path)
-
-    for id in tweetsSortedByLikes[screen_name]:
-        print(user_data[id])
+    print(user_data)
