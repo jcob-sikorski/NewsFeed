6c844687f68a990d5eb30cd7ff02dbd5e44b4ff8
jcob-sikorski <jm.siekiera@gmail.com>
2020-04-10 13:37:54 +0200

    Added sorting functionality with better performance and a function - I will change a way of sorting soon

---
+++ newsfeed/Include/sortTweetsByLikes.py
@@ -0,0 +1,34 @@
+def sortTweetsByLikes(return_tweets=False):
+    # codecs for better performance
+    import json, codecs
+    from jsonLoad import jsonLoad
+    from jsonSave import jsonSave
+
+    friends = jsonLoad('friends_data.json')
+    sorted_tweets_by_likes = {}
+    dir_path = r'C:\Users\jmsie\Dev\Projects\NewsFeed\data'
+
+
+    for screen_name in friends['friends']:
+        path = ''.join(dir_path + f'\{screen_name}_tweets.json')
+        user_data = jsonLoad(path)
+
+        IDs = [*user_data]
+        likes = {}
+
+        # make dictionary with ID: likes pair
+        for ID in IDs:
+            likes.update({ID: user_data[ID]['favorite_count']})
+
+        sorted_IDs_by_likes = list({
+            ID: likes_count 
+            for ID, likes_count in sorted(
+                likes.items(), key=lambda item: int(item[1]), reverse=True)
+                }.keys())
+
+        sorted_tweets_by_likes[screen_name] = {id: user_data[id] for id in sorted_IDs_by_likes}
+
+    with open('sorted_tweets_by_likes.json', 'wb') as f:
+        json.dump(sorted_tweets_by_likes, codecs.getwriter('utf-8')(f), ensure_ascii=False)
+
+sortTweetsByLikes()
